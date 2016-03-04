726a654ad013e913e4d1406b56a0b8d9dc67aec6
tianye0032 <tianye0032@gmail.com>
2016-03-04 17:40:59 -0600

    Change to ADD

--- server/central/CentralServer.java
+++ server/central/CentralServer.java
@@ -111,7 +111,7 @@ public class CentralServer extends Thread{
 				
 				
 				if(config.isFromCentral(message)){
-					if ((!indexTree.isNewVersion(version)||(config.getType(message)!=JobType.UPDATE)))
+					if ((!indexTree.isNewVersion(version)||(config.getType(message)!=JobType.ADD)))
 {
 						System.out.println("Duplicate Message Ignored!   "+version.getFile()+version.getVersionId());
 						return;
